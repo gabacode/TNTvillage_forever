d1d27cee782d0d11464ca6831899bd3d16bc1a61
Gabriele Arcangelo Scalici <gaba41@gmail.com>
2023-08-14 01:46:49 +0200

    style: add link

--- frontend/src/components/organisms/Header/index.tsx
+++ frontend/src/components/organisms/Header/index.tsx
@@ -8,7 +8,9 @@ interface Props {
 export const Header = ({ title, subtitle }: Props) => (
   <header className="position-absolute top-0 start-0 w-100 d-flex align-items-center">
     <div className="container d-flex align-items-center">
-      <img src={headBanner} alt="TNT Village" />
+      <a href="/" className="text-decoration-none">
+        <img src={headBanner} alt="TNT Village" />
+      </a>
       <div className="ms-3">
         <h1 className="h4 fw-bold mb-0">{title}</h1>
         <p className="mt-0 h6 fw-bold">{subtitle}</p>
